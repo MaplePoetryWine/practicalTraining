72ebae5b58ac845fbd95fc07da2756b1af7e0e21
Lx0815 <wzhao2002@gmail.com>
2022-06-24 20:44:42 +0800

    AccountService#register 新增了身份证号唯一验证。

--- AWT/src/com/atAWT/service/AccountService.java
+++ AWT/src/com/atAWT/service/AccountService.java
@@ -1,12 +1,18 @@
 package com.atAWT.service;
 
 import com.atAWT.model.Account;
+import com.atAWT.model.SavingAccount;
 import com.atAWT.model.U;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author: Ding
  * @date: 2022/6/24
- * @description:
+ * @description: 处理 用户相关请求
  * @modify:
  */
 
@@ -35,10 +41,22 @@ public class AccountService {
      * @return
      */
     public Account register(Account account) {
+        HashMap<Integer, SavingAccount> savingMap = U.savingMap;
+        Collection<SavingAccount> savingAccounts = savingMap.values();
+        Set<String> set = new HashSet<>();
+        for (SavingAccount savingAccount : savingAccounts) {
+            set.add(savingAccount.getPersonID());
+        }
+
+        if (! set.add(account.getPersonID())) {
+            return null;
+        }
+
         if (U.addCount(account)) {
             return account;
         } else {
             return null;
         }
+
     }
 }
