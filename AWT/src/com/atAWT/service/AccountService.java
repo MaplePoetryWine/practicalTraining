d0aa11e6e21e6fa70c64f4acdc32fa217b6ec257
Lx0815 <wzhao2002@gmail.com>
2022-06-24 23:50:54 +0800

    AccountService 新增 getAllAccount()

--- AWT/src/com/atAWT/service/AccountService.java
+++ AWT/src/com/atAWT/service/AccountService.java
@@ -93,4 +93,11 @@ public class AccountService {
         }
         return false;
     }
+
+    /**
+     * @return 返回所有已注册的用户
+     */
+    public Collection<Account> getAllAccount() {
+        return AccountDaoImpl.map.values();
+    }
 }
