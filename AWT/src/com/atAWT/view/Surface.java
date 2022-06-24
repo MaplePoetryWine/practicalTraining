adfbaed1e326bc949b247c0ecb70d9bafc736bbd
lyggwsp <lyggwsp@163.com>
2022-06-24 16:06:02 +0800

    wsp 24 11:25

--- AWT/src/com/atAWT/view/Surface.java
+++ AWT/src/com/atAWT/view/Surface.java
@@ -7,11 +7,11 @@ import javax.swing.*;
  * @create 2022-06-24 15:53
  */
 public class Surface {
-    private JButton 查看贷款余额Button;
-    private JButton 贷款Button;
-    private JButton 取款Button;
-    private JButton 转账Button;
-    private JButton 查看个人信息Button;
-    private JButton 修改个人信息Button;
-    private JButton 查看余额Button;
+    private JButton ViewLoanBalanceButton;
+    private JButton loanButton;
+    private JButton withdrawalButton;
+    private JButton TransferButton;
+    private JButton viewPersonalInformationButton;
+    private JButton modifyPersonalInformationButton;
+    private JButton checkBalanceButton;
 }
