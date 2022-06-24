ea3d8e2ce06545a1c16de16cc46c159311d41cc4
lyggwsp <lyggwsp@163.com>
2022-06-24 16:03:01 +0800

    wsp 24 11:25

---
+++ AWT/src/com/atAWT/view/Regist.java
@@ -0,0 +1,14 @@
+package com.atAWT.view;
+
+import javax.swing.*;
+
+/**
+ * @author wspstart
+ * @create 2022-06-24 15:44
+ */
+public class Regist {
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton 登录Button;
+    private JButton 注册Button;
+}
