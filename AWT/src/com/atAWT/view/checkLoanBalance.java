a74e9a28184525809246772ccdafa5a0dc749b93
lyggwsp <lyggwsp@163.com>
2022-06-26 00:12:05 +0800

    AccountService 新增     /**      *      * @param accountId 要查询用户的 id      * @return 返回该用户的余额      */     public double getBalanceById(Integer accountId)

---
+++ AWT/src/com/atAWT/view/checkLoanBalance.java
@@ -0,0 +1,26 @@
+package com.atAWT.view;
+
+import com.atAWT.model.LoanAccount;
+import com.atAWT.service.LoanAccountService;
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
+import javax.swing.*;
+
+/**
+ * @author wspstart
+ * @create 2022-06-25 23:21
+ */
+public class checkLoanBalance extends JFrame {
+    private JTextField IDField1;
+    private JTextField moneytextField2;
+    private JPanel checkPanel;
+    private LoanAccountService loanAccountService = new LoanAccountService();
+
+    public checkLoanBalance(LoanAccount account) {
+        this.setVisible(true);
+        this.add(checkPanel);
+        this.setSize(500,600);
+        IDField1.setText(account.getID().toString());
+        moneytextField2.setText(String.valueOf(loanAccountService.selectLoanAmount(account.getID())));
+    }
+}
