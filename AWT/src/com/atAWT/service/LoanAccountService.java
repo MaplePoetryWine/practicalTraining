60b61be60a50d4808a6ef8738e6cf62f3da5813e
Lx0815 <wzhao2002@gmail.com>
2022-06-25 22:35:40 +0800

    LoanAccountService 新增 1. openLoanAccount 2. selectLoanAccountById 3. loan 4. repayment

---
+++ AWT/src/com/atAWT/service/LoanAccountService.java
@@ -0,0 +1,94 @@
+package com.atAWT.service;
+
+import com.atAWT.model.Account;
+import com.atAWT.model.LoanAccount;
+import com.atAWT.model.U;
+
+/**
+ * @author: Ding
+ * @date: 2022/6/25
+ * @description:
+ * @modify:
+ */
+
+public class LoanAccountService {
+
+    private AccountService accountService = new AccountService();
+
+    /**
+     * 给用户开通贷款账户
+     * @param accountId 要开通贷款账户的 id
+     * @return 返回开通的贷款账户。若为 null 表示其贷款账户已开通，请调用 {@link #selectLoanAccountById(Integer accountId)}
+     */
+    public LoanAccount openLoanAccount(Integer accountId) {
+        Account account = accountService.selectById(accountId);
+        LoanAccount loanAccount = selectLoanAccountById(accountId);
+        if (loanAccount == null) {
+            LoanAccount newLoanAccount = new LoanAccount(account);
+            U.loanMap.put(accountId, newLoanAccount);
+            U.write();
+            return newLoanAccount;
+        }
+        return null;
+    }
+
+    /**
+     * 根据 id 查询已经开通的贷款账户
+     * @param accountId 要查询的贷款账户的 id
+     * @return 返回查询到的 贷款账户，若为 null 表示该用户未开通贷款账户
+     */
+    public LoanAccount selectLoanAccountById(Integer accountId) {
+        U.load();
+        return U.loanMap.get(accountId);
+    }
+
+    /**
+     * 进行贷款操作
+     * @param accountId 要贷款的 id
+     * @param amount 要贷款的 金额
+     * @return 返回是否贷款成功
+     */
+    public boolean loan(Integer accountId, double amount, String password) {
+
+        LoanAccount account = selectLoanAccountById(accountId);
+        if (account == null) return false;
+        if (! password.equals(account.getPassword())) return false;
+
+        double loanAmount = account.getLoanAmount();
+        if (loanAmount > amount) {
+            boolean deposit = accountService.deposit(accountId, account.getPassword(), amount);
+            if (deposit) {
+                account.setLoanAmount(loanAmount - amount);
+                U.load();
+                U.write();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 还款操作
+     * @param accountId 要还款的 用户 id
+     * @param amount 要还款的金额
+     * @param password 密码
+     * @return 返回是否还款成功
+     */
+    public boolean repayment(Integer accountId, double amount, String password) {
+        LoanAccount account = selectLoanAccountById(accountId);
+        if (account == null) return false;
+        if (! password.equals(account.getPassword())) return false;
+
+        double balance = account.getBalance();
+        if (balance > amount) {
+            boolean b = accountService.withdrawMoney(accountId, account.getPassword(), amount);
+            if (b) {
+                account.setLoanAmount(account.getLoanAmount() + amount);
+                U.load();
+                U.write();
+                return true;
+            }
+        }
+        return false;
+    }
+}
