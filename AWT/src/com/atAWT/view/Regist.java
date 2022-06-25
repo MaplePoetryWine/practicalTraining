e8552fddbc2481624b79bc3a110d18280f77cbad
Lx0815 <wzhao2002@gmail.com>
2022-06-25 11:25:36 +0800

    优化 Regist 注册时的逻辑判断

--- AWT/src/com/atAWT/view/Regist.java
+++ AWT/src/com/atAWT/view/Regist.java
@@ -36,8 +36,12 @@ public class Regist extends JFrame{
                 super.mouseClicked(e);
                 Account account = new Account(passwordField1.getText(), nameField2.getText(), PersonIDField.getText(), telField3.getText());
                 Account register = accountS.register(account);
-                if (register == null && personIdIsTrue){
-                    error.setText("注册失败!请检查身份证号是否输入正确。");
+                if (! personIdIsTrue) {
+                    error.setText("请检查身份证号是否输入正确。");
+                    return;
+                }
+                if (register == null){
+                    error.setText("注册失败!");
                 } else {
                     error.setText(" 您的账号为：" + account.getID().toString());
                 }
